86b562a9c70b6787355463e6a1cd4d9b52a0d988
takuya0524 <mrtky1029@icloud.com>
2021-05-19 20:40:00 +0900

    商品出品機能３

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,7 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput.value);
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue*0.1)
     const profitNumber = document.getElementById("profit");
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,7 +38,17 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "State is not a number"
       end
+      it '商品の状態について1が選択された場合保存できないこと' do
+        @item.state_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "State is not a number"
+      end
       it '配送料の負担についての情報が必須であること' do
+        @item.burden_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Burden is not a number"
+      end
+      it '配送料の負担について1が選択された場合保存できないこと' do
         @item.burden_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include "Burden is not a number"
@@ -48,7 +58,17 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Prefecture is not a number"
       end
+      it '発送元の地域について1が選択された場合保存できないこと' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture is not a number"
+      end
       it '発送までの日数についての情報が必須であること'do
+        @item.days_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Days is not a number"
+      end
+      it '発送までの日数について1が選択された場合保存できないこと'do
         @item.days_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include "Days is not a number"
@@ -59,7 +79,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price can't be blank", "Price is not a number"
       end
       it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
       end
@@ -69,9 +89,8 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
         it '販売価格は、¥10,000,000以上の場合保存できないこと' do
-        @item.price = '100000000'
+        @item.price = 100000000
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
       end
     end
