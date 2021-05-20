39c5e145fdeaae418a199f452d471c140c1e0c0e
takuya0524 <mrtky1029@icloud.com>
2021-05-20 20:36:51 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
 
   def index
@@ -20,16 +21,30 @@ class ItemsController < ApplicationController
     end
   end
 
-    def show
-      @item = Item.find(params[:id])
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
     end
-
-
+  end
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :state_id, :burden_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
